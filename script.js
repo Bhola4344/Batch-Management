fd6bf5161ed6e7f984a80b7051739df2d4f0c95c
Bhola4344 <128402806+Bhola4344@users.noreply.github.com>
2026-08-21 15:24:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-      const CONFIG = { WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzEwVkk1Grwu_QGkb1yoP5e8Clr20nK4AYDHJ0rQ18g7cacOi6S7DrhfrPm3tEDPK7VOw/exec', API_TOKEN: 'a8f3k9x2m4p7q1w5e6r8t3y9u2i4o7' };
+      const CONFIG = { WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxnPbElt-bAcTpDiEGRQETGwDXvRu2o73RztGZvH3IRScm_9Zt2V5F6qRZEWjy7Z4OcfA/exec', API_TOKEN: 'a8f3k9x2m4p7q1w5e6r8t3y9u2i4o7' };
       let receiving = [], issues = [], editIndex = null, editIssueIndex = null, isSaving = false, pendingPartyIdx = null, pendingCancelIssueIdx = null, pendingIQCIdx = null, pendingIQCStatus = null;
       let batchMailExcluded = new Set();
       let sessionToken = null, idleTimer = null;
